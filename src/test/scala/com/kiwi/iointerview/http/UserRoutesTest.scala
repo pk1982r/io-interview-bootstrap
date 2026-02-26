b537c559c9b6c7b96a061148f90f8cb3c12b5c65
pk <p.kwiecien@newmotion.com>
2026-02-26 13:49:03 +0100

    Auth middleware tested - timeout from DB.

--- src/test/scala/com/kiwi/iointerview/http/UserRoutesTest.scala
+++ src/test/scala/com/kiwi/iointerview/http/UserRoutesTest.scala
@@ -2,6 +2,7 @@ package com.kiwi.iointerview.http
 
 import cats.effect.*
 import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.testkit.TestControl
 import cats.implicits.catsSyntaxOptionId
 import com.kiwi.iointerview
 import com.kiwi.iointerview.db.UserRepository
@@ -16,6 +17,8 @@ import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.slf4j.Slf4jFactory
+
+import scala.concurrent.duration.DurationInt
 
 // TODO tests with failures
 class UserRoutesTest
@@ -47,16 +50,31 @@ class UserRoutesTest
     } yield response.status.code shouldBe 404
   }
 
-  "it should response in case of error from DB" in withStub { stubUserRepository =>
-    val userRoutes = new UserRoutes(stubUserRepository)
-    val req = Request[IO](Method.GET, uri"/user/1")
-    for {
-      _ <- stubUserRepository.findById.raisesErrorWith(new Throwable("DB error"))
-      response <- userRoutes.routes.orNotFound(req)
-    } yield response.status.code shouldBe 500
+  "it should response in case of error from DB" in withStub {
+    stubUserRepository =>
+      val userRoutes = new UserRoutes(stubUserRepository)
+      val req = Request[IO](Method.GET, uri"/user/1")
+      for {
+        _ <- stubUserRepository.findById.raisesErrorWith(
+          new Throwable("DB error")
+        )
+        response <- userRoutes.routes.orNotFound(req)
+      } yield response.status.code shouldBe 500
   }
 
-  // TODO "it should response in case of timeout from DB"
+  "it should response in case of timeout from DB" in withStub {
+    stubUserRepository =>
+      val userRoutes = new UserRoutes(stubUserRepository)
+      val req = Request[IO](Method.GET, uri"/user/1")
+      val program = for {
+        _ <- stubUserRepository.findById.returnsIOWith(
+          IO.sleep(10.seconds) *> IO(iointerview.user.some)
+        )
+        response <- userRoutes.routes.orNotFound(req)
+      } yield response
+
+      TestControl.executeEmbed(program).asserting(_.status.code shouldBe 500)
+  }
 
   def withStub(test: Stub[UserRepository] => IO[Assertion]): IO[Assertion] = {
     val stubUserRepository = stub[UserRepository]
